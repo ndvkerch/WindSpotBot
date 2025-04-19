028e36320791cca5b59f3fb2965cfffd5100ef77
Nazarenko Denis <ndvkerch@gmail.com>
2025-04-19 20:08:26 +0300

    для прохождения теста в git

--- src/config/topics.py
+++ src/config/topics.py
@@ -3,6 +3,7 @@ from typing import Dict
 
 class TopicConfig:
     """Конфигурация тем для спотов."""
+
     SPOT_TOPICS: Dict[str, int] = {
         "Должанка": 0,  # Замени на реальный message_thread_id
         "Ейск": 0,
