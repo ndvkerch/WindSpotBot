5d39c1972de41028080cb4fb77a55330b151f544
Nazarenko Denis <ndvkerch@gmail.com>
2025-04-19 17:26:25 +0300

    Добавлен конфиг топиков для message_thread_id спота

---
+++ src/config/topics.py
@@ -0,0 +1,9 @@
+from typing import Dict
+
+class TopicConfig:
+    """Конфигурация тем для спотов."""
+    SPOT_TOPICS: Dict[str, int] = {
+        "Должанка": 0,  # Замени на реальный message_thread_id
+        "Ейск": 0,
+        "Анапа": 0,
+    }
