ffad8e5ceb75f53d664388fc4262c35302cf1d84
Nazarenko Denis <ndvkerch@gmail.com>
2025-04-19 20:11:07 +0300

    Обновление NotificationService и ChatService для работы с TopicService

--- src/services/chat.py
+++ src/services/chat.py
@@ -1,14 +1,20 @@
 from aiogram import Bot
 from typing import List
+from services.topic import TopicService
+from config import settings
 
 
 class ChatService:
     """Сервис для управления чатами спотов."""
 
-    def __init__(self, bot: Bot):
+    def __init__(self, bot: Bot, topic_service: TopicService):
         self.bot = bot
+        self.topic_service = topic_service
 
-    async def filter_messages(self, spot_name: str, limit: int) -> List[dict]:
-        """Фильтрация сообщений по хэштегу спота."""
-        # TODO: Реализовать логику
+    async def get_spot_messages(self, spot_name: str, limit: int) -> List[dict]:
+        """Получение сообщений из темы спота."""
+        thread_id = await self.topic_service.get_topic_id(spot_name)
+        if thread_id:
+            # TODO: Реализовать получение сообщений из темы
+            return []
         return []
--- src/services/notification.py
+++ src/services/notification.py
@@ -1,14 +1,22 @@
 from aiogram import Bot
 from models.user import User
+from services.topic import TopicService
+from config import settings
 
 
 class NotificationService:
     """Сервис для отправки уведомлений."""
 
-    def __init__(self, bot: Bot):
+    def __init__(self, bot: Bot, topic_service: TopicService):
         self.bot = bot
+        self.topic_service = topic_service
 
     async def send_checkin_notification(self, user: User, spot_name: str):
-        """Отправка уведомления о чек-ине."""
-        # TODO: Реализовать логику
-        pass
+        """Отправка уведомления о чек-ине в тему спота."""
+        thread_id = await self.topic_service.get_topic_id(spot_name)
+        if thread_id:
+            await self.bot.send_message(
+                chat_id=settings.CHAT_ID,
+                message_thread_id=thread_id,
+                text=f"@{user.username} отметился на споте '{spot_name}'",
+            )
