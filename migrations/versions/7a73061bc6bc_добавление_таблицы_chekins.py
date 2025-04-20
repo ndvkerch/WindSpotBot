64118b150465f658ea8334b601c6b9d4f9973024
Nazarenko Denis <ndvkerch@gmail.com>
2025-04-20 10:53:58 +0300

    Обновлена миграция checkins для связи с spots.id

---
+++ migrations/versions/7a73061bc6bc_добавление_таблицы_chekins.py
@@ -0,0 +1,36 @@
+"""Добавление таблицы chekins
+
+Revision ID: 7a73061bc6bc
+Revises: 6fcea6a1e1dc
+Create Date: 2025-04-20 10:52:13.850979
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "7a73061bc6bc"
+down_revision: Union[str, None] = "6fcea6a1e1dc"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    op.create_table(
+        "checkins",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("user_id", sa.Integer, nullable=False),
+        sa.Column("spot_id", sa.Integer, nullable=False),
+        sa.Column("checkin_type", sa.Integer, nullable=False),
+        sa.Column("created_at", sa.DateTime, nullable=False),
+        sa.Column("description", sa.String, nullable=True),
+        sa.ForeignKeyConstraint(["spot_id"], ["spots.id"]),
+    )
+
+
+def downgrade():
+    op.drop_table("checkins")
