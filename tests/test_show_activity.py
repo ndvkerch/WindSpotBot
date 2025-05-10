903515e75bc496651d6503ed5408aa41c785753a
Nazarenko Denis <ndvkerch@gmail.com>
2025-05-10 21:00:45 +0300

    Обновлён тест test_show_activity для нового расположения show_activity

--- tests/test_show_activity.py
+++ tests/test_show_activity.py
@@ -1,11 +1,12 @@
 import pytest
-from src.bot import show_activity
+from src.handlers.activity import show_activity
 from aiogram.fsm.context import FSMContext
 from unittest.mock import AsyncMock
 from src.services.spot import SpotService
 from src.services.checkin import CheckinService
 from src.services.weather import WeatherService
 from src.services.chat import ChatService
+from src.services.geo import GeoService
 
 
 @pytest.mark.asyncio
@@ -15,6 +16,7 @@ async def test_show_activity_no_spots():
     checkin_service = AsyncMock(spec=CheckinService)
     weather_service = AsyncMock(spec=WeatherService)
     chat_service = AsyncMock(spec=ChatService)
+    geo_service = AsyncMock(spec=GeoService)
     state = AsyncMock(spec=FSMContext)
     spot_service.get_all_spots.return_value = []
     await show_activity(
@@ -27,5 +29,6 @@ async def test_show_activity_no_spots():
         chat_service,
         7395203712,
         state,
+        geo_service,
     )
     message.answer.assert_called_with("Активные споты не найдены.")
